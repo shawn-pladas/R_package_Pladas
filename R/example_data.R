58de0a1cf637eeb28806f66e3cb169d91fddee86
shawn <shawn.pladas@gmail.com>
2021-10-26 20:11:44 +0000

    data for crab depths

---
+++ R/example_data.R
@@ -0,0 +1,8 @@
+ggplot(crabs, aes(x = carapace_length, y = body_depth, color = color)) + 
+  geom_point() + 
+  labs(x = "Carapace length (mm)",
+       y = "Body Depth (mm)",
+       color = "Crab color") + 
+  scale_color_manual(values = c("blue", "orange")) + # why not!
+  geom_smooth(method = "lm") +
+  annotate("text", x = 20, y = 30, label = "R^2 == 0.988", parse=T, size=5)
