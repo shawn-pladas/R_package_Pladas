0b3a65f9b1231e85377c5cda1a0c7021ae50af46
shawn <shawn.pladas@selu.edu>
2021-11-16 21:20:01 +0000

    updates

---
+++ R/Mult.R
@@ -0,0 +1,19 @@
+#' Multiply together two numbers
+#' 
+#' @param x A number
+#' @param y Another number
+#' @return product The product of multiplying x and y
+#' @usage
+#' mult(4,5)
+
+mult <- function(x, y){
+  product <- x*y
+  is.numeric(product) == FALSE{
+    return("ERROR - non-numeric")
+  }
+  return(product)
+}
+
+
+# run error messages for funsction in project 3 
+
