bead38c25866fcbc166160dc11261f5336a6011d
shawn <shawn.pladas@selu.edu>
2021-11-30 19:34:23 +0000

    new function

---
+++ R/ANCOVA.R
@@ -0,0 +1,21 @@
+#Input: Data frame with predictor and response columns
+#output: Linear model comparing two predictor columns, numerical and categorical.
+#
+#' Create a multiple predictor linear model using numerical and categorical data sets
+#' 
+#' @param df A data frame
+#' @param response A response variable 
+#' @param predictor1 A numerical predictor variable
+#' @param predictor2 A categorical predictor variable
+#' @return A linear model test summary with of categorical and numerical predictors
+#' @usage
+#' ancova(df, response, predictor1, predictor2)
+
+ancova <- function(df, response, predictor1, predictor2) {
+  ancova_fit <- lm({{response}} ~ {{predictor1}} + {{predictor2}}, data = df)
+  summary(ancova_fit)
+  if (is.data.frame(df) == FALSE){
+    return("ERROR - not a dataframe")
+  }
+  return(summary(ancova_fit))
+}
